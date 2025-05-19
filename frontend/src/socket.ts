901a00be4299ca96e8cf08c504fee89509671556
pablocarril01 <pabloperezcarril@gmail.com>
2025-05-19 10:01:51 +0200

    WS

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,7 +1,10 @@
 import { io } from "socket.io-client";
 
-const socket = io(process.env.URL_BACKEND || "http://localhost:3000", {
-  transports: ["websocket"],
-});
+const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+//const host = window.location.hostname;
+const port = 3000; // o el puerto real del backend
 
-export { socket };
+export const socket = io(`${protocol}://localhost:${port}`, {
+  transports: ["websocket"],
+  path: "/socket.io",
+});
