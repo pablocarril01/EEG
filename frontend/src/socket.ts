b6d037f8b001c14f29ac6eb8985f8d835b5c5927
pablocarril01 <pabloperezcarril@gmail.com>
2025-05-19 10:06:04 +0200

    WS

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,10 +1,10 @@
 import { io } from "socket.io-client";
 
 const protocol = window.location.protocol === "https:" ? "wss" : "ws";
-//const host = window.location.hostname;
+const host = window.location.hostname;
 const port = 3000; // o el puerto real del backend
 
-export const socket = io(`${protocol}://localhost:${port}`, {
+export const socket = io(`${protocol}://${host}:${port}`, {
   transports: ["websocket"],
   path: "/socket.io",
 });
