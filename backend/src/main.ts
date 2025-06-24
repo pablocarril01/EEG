1e402961e0740b8a1981a9774431f0f6010cafbc
pablocarril01 <pabloperezcarril@gmail.com>
2025-06-24 16:51:27 +0200

    A ver

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,22 @@
-import * as dotenv from 'dotenv';
-import * as path from 'path';
-
-dotenv.config({ path: path.resolve(__dirname, '../../.env') });
-
-import 'reflect-metadata';
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import historicoRouter from './historico/historico';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'log', 'debug'],
   });
-  await app.listen(3000, '0.0.0.0');
+
+  // Aquí obtenemos la instancia de Express que usa Nest:
+  const expressApp = app.getHttpAdapter().getInstance();
+  expressApp.use('/api/historico', historicoRouter);
+
   app.enableCors({
     origin: ['http://localhost:3001', 'http://193.146.34.10'],
     credentials: true,
   });
-}
 
+  await app.listen(3000, '0.0.0.0');
+}
 bootstrap();
