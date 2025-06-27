2308afa3c4add367a36100dda9cd721dcb826ef5
pablocarril01 <pabloperezcarril@gmail.com>
2025-06-27 12:31:59 +0200

    Descargar EDF

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService }    from './app.service';
-import { RedisModule }   from './redis/redis.module';
+import { AppService } from './app.service';
+import { RedisModule } from './redis/redis.module';
 import { DatabaseModule } from './database.module';
 import { RedisWatcherService } from './measurements/redis-watcher.service';
-import { EegGateway }    from './gateway/eeg.gateway';
+import { EegGateway } from './gateway/eeg.gateway';
+import { EdfModule } from './edf/edf.module';
 
 @Module({
   imports: [
-    RedisModule,      // tu conexión a Redis original :contentReference[oaicite:0]{index=0}
-    DatabaseModule,   // configura TypeORM y tu entity 'pepi'
+    RedisModule, // tu conexión a Redis original :contentReference[oaicite:0]{index=0}
+    DatabaseModule, // configura TypeORM y tu entity 'pepi'
+    EdfModule,
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [
     AppService,
     EegGateway,
-    RedisWatcherService,  // suscribe a key-space events y vuelca a Postgres
+    RedisWatcherService, // suscribe a key-space events y vuelca a Postgres
   ],
 })
 export class AppModule {}
