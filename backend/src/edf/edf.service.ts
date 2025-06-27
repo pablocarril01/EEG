0d7d26c03706711a2b4d2df2f5c37908215bac14
pablocarril01 <pabloperezcarril@gmail.com>
2025-06-27 12:46:04 +0200

    Descargar EDF

--- backend/src/edf/edf.service.ts
+++ backend/src/edf/edf.service.ts
@@ -6,21 +6,20 @@ import type { Response } from 'express';
 @Injectable()
 export class EdfService {
   generarEdf(paciente: string, desde: string, hasta: string, res: Response) {
-    const scriptPath = join(__dirname, '../scripts/postgres-edf.py');
+    // process.cwd() === /home/grupo-cima/EEG/backend
+    const scriptPath = join(process.cwd(), 'scripts', 'postgres-edf.py');
+
+    console.log('EDF script path:', scriptPath);
+
     const py = spawn('python3', [scriptPath, paciente, desde, hasta]);
 
-    // Cabeceras para descarga
     res.set({
       'Content-Type': 'application/octet-stream',
       'Content-Disposition': `attachment; filename="${paciente}_${desde.replace(/-/g, '')}-${hasta.replace(/-/g, '')}.edf"`,
     });
 
-    // Pipe stdout del script al cliente
     py.stdout.pipe(res);
-
-    py.stderr.on('data', (data) => {
-      console.error('EDF error:', data.toString());
-    });
+    py.stderr.on('data', (d) => console.error('EDF error:', d.toString()));
     py.on('close', (code) => {
       if (code !== 0) {
         throw new InternalServerErrorException('Error generando EDF');
